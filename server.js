9af45d9af47e7672264b6d4a3a44a7f644e38bd5
Josh Nielsen <josh@joshontheweb.com>
2012-11-09 18:21:52 -0700

    got socket.io working

--- server.js
+++ server.js
@@ -2,22 +2,39 @@ var http = require('http'),
     conf = require('./conf.js'),
     express = require('express'),
     _ = require('underscore'),
+    sio = require('socket.io'),
     box2d = require('box2d');
 
-var app = express.createServer();
+var app = express();
+var server = http.createServer(app);
 
 app.configure(function() {
   app.set('views', __dirname + '/templates/');
   app.engine('html', require('ejs').renderFile);
   app.use(express.errorHandler());
   app.use('/media', express.static(__dirname + '/media'));
-  app.use('/', express.static(__dirname + '/templates/'))
+  // app.use('/', express.static(__dirname + '/templates/'))
+  app.use(app.router);
 });
 
 app.get('/', function(req, res, next) {
   res.render('layout.html');
 });
 
-app.listen(conf.port);
+server.listen(conf.port);
 
-console.log('Server running at http://0.0.0.0:' + conf.port);
+console.log('Server running at http://localhost:' + conf.port);
+
+
+// Sockets
+var io = sio.listen(server);
+
+io.sockets.on('connection', function(socket) {
+  io.sockets.emit('someone connected');
+  console.log('someone connected');
+
+  socket.on('disconnect', function() {
+    io.sockets.emit('someone disconnected');
+    console.log('someone disconnected');
+  });
+});
