8679eda55fb9d90b0a72d8b2129c44d9a651696c
Josh Nielsen <josh@joshontheweb.com>
2012-11-09 19:53:27 -0700

    fixed broken socket server

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ console.log('Server running at http://localhost:' + conf.port);
 var io = sio.listen(server);
 
 io.sockets.on('connection', function(socket) {
-  io.sockets.emit('alert', socket);
+  io.sockets.emit('news', 'someone connected');
   console.log('someone connected');
 
   socket.on('disconnect', function() {
-    io.sockets.emit('someone disconnected');
+    io.sockets.emit('news', 'someone disconnected');
     console.log('someone disconnected');
   });
 });
