0a5788836df791c7cac763229ae95085cb691023
Josh Nielsen <josh@joshontheweb.com>
2012-11-09 17:24:10 -0700

    initial server setup

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000);
+var http = require('http'),
+    conf = require('./conf.js'),
+    express = require('express'),
+    _ = require('underscore'),
+    box2d = require('box2d');
 
-console.log('Server running at http://0.0.0.0:8000/');
+var app = express.createServer();
+
+app.configure(function() {
+  app.set('views', __dirname + '/templates/');
+  app.use(express.errorHandler());
+  app.use('/media', express.static(__dirname + '/media'));
+  app.use('/', express.static(__dirname + '/templates/'))
+});
+
+app.get('/', function(req, res, next) {
+  res.send('Hello World');
+});
+
+app.listen(conf.port);
+    
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(8000);
+
+console.log('Server running at http://0.0.0.0:' + conf.port);
