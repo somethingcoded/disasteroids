a23e0298d729d61282464d1f972e922360b692f7
Josh Nielsen <josh@joshontheweb.com>
2012-11-09 19:55:37 -0700

    world is rendering

--- media/scripts/init.js
+++ media/scripts/init.js
@@ -4,7 +4,8 @@
   window.socket = socket = io.connect();
 
   window.world = new sc.models.World();
-  window.worldView = new sc.views.WorldView({model: window.world});
+  window.worldView = new sc.views.WorldView({model: window.world, el: $('.main')});
+  window.worldView.render();
 
   socket.on('sync', function(data) {
     window.world.start(data);
