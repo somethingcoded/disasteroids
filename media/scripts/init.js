46d9b864069ee766242293cb97a3d30786138329
Josh Nielsen <josh@joshontheweb.com>
2012-11-09 19:37:42 -0700

    scripts are loaded in layout.html

--- media/scripts/init.js
+++ media/scripts/init.js
@@ -3,7 +3,7 @@
   var socket;
   window.socket = socket = io.connect();
 
-  window.world = new sc.models.World(data);
+  window.world = new sc.models.World();
   window.worldView = new sc.views.WorldView({model: window.world});
 
   socket.on('sync', function(data) {
