f3d6040b089eb42347438d202ccd63a219f01381
Michael Toymil <mike.toymil@gmail.com>
2012-11-09 20:36:44 -0700

    renderTest

--- media/scripts/mvc/views/world_view.js
+++ media/scripts/mvc/views/world_view.js
@@ -10,7 +10,23 @@
 
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
+      this.renderTest();
       return this;
+    },
+
+    renderTest: function() {
+      var canvas = new fabric.Canvas(this.$el.find('canvas')[0])
+      canvas.backgroundColor = 'black';
+      fabric.loadSVGFromURL('/media/art/asteroid_01.svg', function(objects) { 
+          var group = new fabric.PathGroup(objects, { 
+          left: 100, 
+          top: 100, 
+          scaleX: .5,
+          scaleY: .5
+        }); 
+        canvas.add(group); 
+        canvas.renderAll(); 
+          }); 
     }
     
   });
