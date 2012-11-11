179f8a4f7ada2cbb1a67e32ea2da51ec3fbd3025
Josh Nielsen <josh@joshontheweb.com>
2012-11-11 13:34:02 -0700

    added support for firefox audio

--- media/scripts/mvc/models/app.js
+++ media/scripts/mvc/models/app.js
@@ -3,6 +3,7 @@
 
   sc.models.App = Backbone.Model.extend({
     initialize: function() {
+      this.detectAudioSupport();
       this.chatLog = new sc.models.ChatLog();
     },
 
@@ -22,6 +23,27 @@
       Backbone.history.start({pushState: true});
     },
 
+    audioSuffix: undefined,
+
+    detectAudioSupport: function() {
+      var suffix = this.suffix = undefined;
+      var audio = document.createElement('audio');
+      if (audio.canPlayType('audio/mpeg;')) {
+        suffix = 'mp3';
+      } else {
+        suffix = 'ogg';
+      }    
+      
+      this.audio = {
+        rocketShoot: $('<audio src="/media/audio/rocket_shoot.' + suffix + '"></audio>')[0],
+        rocketImpact: $('<audio src="/media/audio/rocket_impact.' + suffix + '"></audio>')[0],
+        asteroidExplosion: $('<audio src="/media/audio/asteroid_explosion.' + suffix + '"></audio>')[0],
+        playerExplosion: $('<audio src="/media/audio/player_explosion.' + suffix + '"></audio>')[0],
+        walk: $('<audio src="/media/audio/walk.' + suffix + '"></audio>')[0],
+        jump: $('<audio src="/media/audio/jump.' + suffix + '"></audio>')[0],
+      }
+    },
+
     audioEnabled: true,
 
     playAudio: function(name) {
@@ -30,14 +52,6 @@
       }
     },
 
-    audio: {
-      rocketShoot: $('<audio src="/media/audio/rocket_shoot.mp3"></audio>')[0],
-      rocketImpact: $('<audio src="/media/audio/rocket_impact.mp3"></audio>')[0],
-      asteroidExplosion: $('<audio src="/media/audio/asteroid_explosion.mp3"></audio>')[0],
-      playerExplosion: $('<audio src="/media/audio/player_explosion.mp3"></audio>')[0],
-      walk: $('<audio src="/media/audio/walk.mp3"></audio>')[0],
-      jump: $('<audio src="/media/audio/jump.mp3"></audio>')[0],
-    }
   });
   
 })();
